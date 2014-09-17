0a9fc79d04376a0759f0017ccd8ff0475d1b3eb3
Miriam <miriam.leah.teitelbaum@gmail.com>
2014-09-16 22:20:38 -0400

    fixed ResponseServer
    changes while loop conditional

--- src/teitelbaum/network/ResponseServer.java
+++ src/teitelbaum/network/ResponseServer.java
@@ -13,21 +13,34 @@ public class ResponseServer
 	public static void main(String[] args) throws IOException
 	{
 		ServerSocket serverSocket = new ServerSocket(8080); //provide port
-		Socket socket = serverSocket.accept(); //to listen
 
-		//read
-		InputStream in = socket.getInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		String line;
-		while ((line = reader.readLine()) != null)
+		int counter = 0;
+		while (true) //want to continuously accept requests from many clients
 		{
-			System.out.println(line);
+			Socket socket = serverSocket.accept(); //to listen
+
+			//read
+			InputStream in = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			String line;
+			while (!"".equals((line = reader.readLine()))) //readline is a blocking call so if nothing there will block and stop execution until gets input, therefore check for empty string since its the last line signature
+			{
+				System.out.println(line);
+			}
+
+			//write 
+			OutputStream out = socket.getOutputStream();
+			String response = "<h2>This is request " + counter + " </h2>";
+			out.write("HTTP/1.1 200 OK\n".getBytes());
+			out.write("Content-Type: text/html; charset=utf-8\n".getBytes());
+			out.write(("Content-Length: " + response.length() + "\n\n").getBytes());
+			out.write(response.getBytes());
+			out.flush(); //sends out the data
+			out.close();
+
+			counter++;
 		}
 
-		//write 
-		OutputStream out = socket.getOutputStream();
-		out.write("hello world!".getBytes());
-		out.flush(); //sends out the data
-		out.close();
 	}
 }
